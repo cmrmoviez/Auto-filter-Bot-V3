4244679d49d34e202fd80bb43a075080c7134432
cmr <cmrmoviez@gmail.com>
2024-05-30 14:59:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -143,7 +143,7 @@ URL = environ.get("URL", "https://mytestvj-998c9929dc7a.herokuapp.com/")
 RENAME_MODE = bool(environ.get('RENAME_MODE', True)) # Set True or Flase
 
 # Auto Approve Info : If True Then Bot Approve New Upcoming Join Request Else Not
-AUTO_APPROVE_MODE = bool(environ.get('AUTO_APPROVE_MODE', True)) # Set True or Flase
+AUTO_APPROVE_MODE = bool(environ.get('AUTO_APPROVE_MODE', False)) # Set True or Flase
 
 # Save Restricted Info : If True Then Bot Save Content From Restricted Channel Else Not
 SAVE_RESTRICTED_MODE = bool(environ.get('SAVE_RESTRICTED_MODE', False)) # Set True or Flase
